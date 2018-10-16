2a1f8173d0fcbb086c0063b87a896eb59b442804
drag13 <nereality13@mail.ru>
2018-10-16 20:08:33 +0300

    add button firstGeneration

--- src/GoL/Field.java
+++ src/GoL/Field.java
@@ -1,20 +1,21 @@
 package GoL;
 
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
+
+
+/*
 
 public class Field {
 
 
-    public void firstGeneation(boolean[][] graph) {
 
-        Random rand = new Random();
-        for (int i = 0; i < graph.length; i++) {
-            for (int j = 0; j < graph[i].length; j++) {
-                boolean r = rand.nextBoolean();
-                graph[i][j] = r;
-            }
 
-        }
-    }
+
+
 }
+*/
--- src/GoL/Game.java
+++ src/GoL/Game.java
@@ -1,20 +1,25 @@
 package GoL;
 
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 import javax.swing.*;
 
 
 public class Game {
-    private JFrame frame;
-    Canvas canvasPanel;
-    final String NAME_OF_GAME="Conway-s-Game-of-Life";
+
+    final String NAME_OF_GAME="Conway's Game of Life";
     final int START_LOCATION=200;
     final int BTN_PANEL_HEIGHT=58;
     final int MASS_SIZE=50;
     final int POINT_RADIUS=10;
     final int FIELD_SIZE=(MASS_SIZE+1)*POINT_RADIUS-3;
-    boolean[][]oldGeneration=new boolean[MASS_SIZE][MASS_SIZE];
-    boolean[][]newGeneration=new boolean[MASS_SIZE][MASS_SIZE];
+   boolean[][]oldGeneration=new boolean[MASS_SIZE][MASS_SIZE];
+   boolean[][]newGeneration=new boolean[MASS_SIZE][MASS_SIZE];
+    Canvas canvasPanel;
+    JFrame frame;
 
 
     public static void main(String[] args) {
@@ -34,11 +39,53 @@ public class Game {
         canvasPanel=new Canvas();
         canvasPanel.setBackground(Color.WHITE);
 
+        JButton fillButton=new JButton("Fill");
+        fillButton.addActionListener(new FillButtonListener());
+        JButton stepButton=new JButton("next");
+
+
 
         JPanel btnPanel=new JPanel();
+        btnPanel.add(fillButton);
+        btnPanel.add(stepButton);
+
+
+        frame.getContentPane().add(BorderLayout.CENTER,canvasPanel);
+        frame.getContentPane().add(BorderLayout.SOUTH,btnPanel);
 
 
         frame.setVisible(true);
     }
+    public class FillButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent av) {
+
+            Random rand = new Random();
+            int countGeneration = 1;
+            for (int x = 0; x < MASS_SIZE; x++) {
+                for (int y = 0; y < MASS_SIZE; y++) {
+                    oldGeneration[x][y] = rand.nextBoolean();
+                }
+            }
+
+            canvasPanel.repaint();
+        }
+    }
+
+    public class Canvas extends JPanel {
+        @Override
+        public void paint(Graphics g){
+            super.paint(g);
+            for(int x=0;x<MASS_SIZE;x++){
+                for(int y=0;y<MASS_SIZE;y++){
+                    if(oldGeneration[x][y]){
+                        g.fillOval(x*POINT_RADIUS,y*POINT_RADIUS,POINT_RADIUS,POINT_RADIUS);
+                    }
+                }
+            }
+        }
+
+    }
+
 }
+
 
